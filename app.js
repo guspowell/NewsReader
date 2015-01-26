340b0467222f8fa260079f17e18c60b5d62ec066
guspowell <augustuspowell@me.com>
2015-01-26 11:33:02 +0000

    implementing jquery 8/10

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-app.js
+var main = function() {
+    
+    $('.article').click(function() {
+        
+        $('.article').removeClass('current');
+        
+        $('.description').hide();
+        
+        $(this).addClass('current');
+        
+        $(this).children('.description').show();
+        
+    });
+    
+    $(document).keypress(function(event) {
+        if (event.which === 111) {
+            $('.current').children('.description').toggle();
+        }
+    });
+    
+};
+
+$(document).ready(main);
